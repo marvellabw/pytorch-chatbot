42671fb0c975a00e170d5b0c14dbbb3a00718c02
Marvella BW <marvellaaabw@gmail.com>
2024-01-28 10:58:03 +1100

    implemented NeuralNet class in model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch, torch.nn as nn
+
+class NeuralNet(nn.Module):
+	def __init__(self, inputSize, hiddenSize, numClasses) -> None:
+		self.l1 = nn.Linear(inputSize, hiddenSize)
+		self.l2 = nn.Linear(hiddenSize, hiddenSize)
+		self.l3 = nn.Linear(hiddenSize, numClasses)
+		self.relu = nn.ReLU()
+	
+	def forward(self, x):
+		out = self.l1(x)
+		out = self.relu(out)
+		out = self.l2(out)
+		out = self.relu(out)
+		out = self.l3(out)
+		return out
